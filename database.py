5b8fd7f142e25f087c9a9191656f746ddb004db6
Narendra Kumar <narendra.kumar.707033@gmail.com>
2026-05-31 09:34:51 +0530

    Added database script and added more features

---
+++ database.py
@@ -0,0 +1,63 @@
+import sqlite3
+from datetime import datetime
+
+con = sqlite3.connect("relay.db", check_same_thread=False)
+cur = con.cursor()
+
+
+def create_table():
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user TEXT,
+        message TEXT,
+        created_at TEXT
+    )
+    """)
+    con.commit()
+
+
+create_table()
+
+
+def get_all_data():
+    cur.execute("SELECT * FROM messages")
+    return cur.fetchall()
+
+
+def delete_message_by_id(message_id):
+    cur.execute("DELETE FROM messages WHERE id = ?", (message_id,))
+    con.commit()
+    return cur.rowcount > 0
+
+
+def update_message_by_id(message_id, new_message):
+    cur.execute(
+        "UPDATE messages SET message = ? WHERE id = ?", (new_message, message_id)
+    )
+    con.commit()
+    return cur.rowcount > 0
+
+
+def insert_message(user, message):
+    created_at = datetime.now().isoformat()
+    cur.execute(
+        "INSERT INTO messages (user, message, created_at) VALUES (?, ?, ?)",
+        (user, message, created_at),
+    )
+    con.commit()
+
+
+def get_message_by_id(message_id: int):
+    cur.execute("SELECT * FROM messages WHERE id = ?", (message_id,))
+    return cur.fetchone()
+
+
+def find_messages_by_user(username):
+    cur.execute("SELECT * FROM messages WHERE user = ?", (username,))
+    return cur.fetchall()
+
+
+def search_message(message):
+    cur.execute("SELECT * FROM messages WHERE message LIKE ?", ("%" + message + "%",))
+    return cur.fetchall()
--- main.py
+++ main.py
@@ -1,56 +1,113 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from database import (
+    get_all_data,
+    delete_message_by_id,
+    update_message_by_id,
+    insert_message,
+    get_message_by_id,
+    find_messages_by_user,
+    search_message,
+)
 
-
-
-    
 app = FastAPI()
 
-messages_db = []
 
 class Message(BaseModel):
-    id: int
     user: str
     message: str
-    
+
+
+class MessageUpdate(BaseModel):
+    message: str
 
 
 @app.post("/messages")
 def send(message: Message):
-    message_dict = message.dict()
-    messages_db.append(message_dict)    
-    return {"status": "success", "data": message_dict} 
+
+    insert_message(message.user, message.message)
+
+    return {"status": "success", "data": message.dict()}
 
 
 @app.get("/messages")
-def get_messages():
-    return {"messages": messages_db}
-    
-@app.delete("/delete/{message_id}")
+def get_all_messages():
+    data = get_all_data()
+    messages = []
+    for row in data:
+        message_dict = {
+            "id": row[0],
+            "user": row[1],
+            "message": row[2],
+            "created_at": row[3],
+        }
+        messages.append(message_dict)
+    return {"messages": messages}
+
+
+@app.get("/messages/{message_id}")
+def get_one_message(message_id: int):
+    data = get_message_by_id(message_id)
+    if not data:
+        return {"status": "fail", "message": "Message not found"}
+    return {
+        "message": {
+            "id": data[0],
+            "user": data[1],
+            "message": data[2],
+            "created_at": data[3],
+        }
+    }
+
+
+@app.delete("/messages/{message_id}")
 def delete_message(message_id: int):
-    id_found = False
-    for message in messages_db:
-        if message["id"] == message_id:
-            id_found = True
-            messages_db.remove(message) 
-            break
-    if id_found:
-        return {"status": "success"}
-    else:
-        return {"status": "fail"}
-   
-   
-@app.put("/update-message")
-def update_message(message: Message):
-    id_found = False
-    message_dict = message.dict()
-    for old_message in messages_db:
-        if old_message["id"] == message_dict["id"]:
-            old_message["message"] = message_dict["message"]            
-            id_found = True
-            break
-    if id_found:
-        return {"status": "success"}
-    else:
-        return {"status": "fail"}
-    
+    deleted = delete_message_by_id(message_id)
+    if not deleted:
+        return {"status": "fail", "message": "Message not found"}
+    return {"status": "success", "deleted_id": message_id}
+
+
+@app.put("/messages/{message_id}")
+def update_message(message_id: int, message: MessageUpdate):
+    updated = update_message_by_id(message_id, message.message)
+    if not updated:
+        return {"status": "fail", "message": "Message not found"}
+    return {
+        "status": "success",
+        "updated_id": message_id,
+        "new_message": message.message,
+    }
+
+
+@app.get("/users/{username}/messages")
+def get_only_user_message(username: str):
+    data = find_messages_by_user(username)
+    if not data:
+        return {"status": "fail", "message": "user not found"}
+    messages = []
+    for row in data:
+        messages.append(
+            {"id": row[0], "user": row[1], "message": row[2], "created_at": row[3]}
+        )
+
+    return {"messages": messages}
+
+
+@app.get("/search/")
+def search_messages(text: str | None = None):
+    if not text:
+        return {"status": "fail", "message": "no search query"}
+
+    data = search_message(text)
+
+    if not data:
+        return {"status": "fail", "message": "No messages found"}
+
+    messages = []
+    for row in data:
+        messages.append(
+            {"id": row[0], "user": row[1], "message": row[2], "created_at": row[3]}
+        )
+
+    return {"messages": messages}
